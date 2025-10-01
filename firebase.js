1fef2b1e7feaaa4e7beaba14a5d4f78bc74376d8
USDT2030 <fadifadi203074@gmail.com>
2025-10-01 13:27:18 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,15 +1,21 @@
-// استبدل القيم بقيم مشروعك على Firebase
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyAzH6gqMdbxKhoZwkWwfJuuopElNRU2JsU",
+  authDomain: "alotebi-3500c.firebaseapp.com",
+  projectId: "alotebi-3500c",
+  storageBucket: "alotebi-3500c.firebasestorage.app",
+  messagingSenderId: "99457641243",
+  appId: "1:99457641243:web:b9e86295f64e4f652b81be",
+  measurementId: "G-HQF5V5FY2L"
 };
 
 // Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-const auth = firebase.auth();
-const db = firebase.firestore();
-const storage = firebase.storage();
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
