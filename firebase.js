1e7edafda6ef3f3fd9774a605e866b7e61ae1d9b
USDT2030 <fadifadi203074@gmail.com>
2025-10-02 12:03:09 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -4,18 +4,18 @@
 <script src="https://www.gstatic.com/firebasejs/9.22.1/firebase-storage-compat.js"></script>
 
 <script>
-const firebaseConfig = {
-  apiKey: "AIzaSyAzH6gqMdbxKhoZwkWwfJuuopElNRU2JsU",
-  authDomain: "alotebi-3500c.firebaseapp.com",
-  projectId: "alotebi-3500c",
-  storageBucket: "alotebi-3500c.appspot.com",
-  messagingSenderId: "99457641243",
-  appId: "1:99457641243:web:b9e86295f64e4f652b81be",
-  measurementId: "G-HQF5V5FY2L"
-};
+  const firebaseConfig = {
+    apiKey: "AIzaSyAzH6gqMdbxKhoZwkWwfJuuopElNRU2JsU",
+    authDomain: "alotebi-3500c.firebaseapp.com",
+    projectId: "alotebi-3500c",
+    storageBucket: "alotebi-3500c.appspot.com",
+    messagingSenderId: "99457641243",
+    appId: "1:99457641243:web:b9e86295f64e4f652b81be",
+    measurementId: "G-HQF5V5FY2L"
+  };
 
-firebase.initializeApp(firebaseConfig);
-const auth = firebase.auth();
-const db = firebase.firestore();
-const storage = firebase.storage();
+  firebase.initializeApp(firebaseConfig);
+  const auth = firebase.auth();
+  const db = firebase.firestore();
+  const storage = firebase.storage();
 </script>
